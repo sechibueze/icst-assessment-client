43226d21df90069b9c53e16f950f6fc14ce87e14
Samuel Chibueze <sechibueze@gmail.com>
2021-03-18 17:10:19 +0100

    work

--- src/views/form.js
+++ src/views/form.js
@@ -20,7 +20,13 @@ const Form = () => {
           console.log("data ", data);
           setCatData(data.data);
         })
-        .catch((err) => setMessage("An error occured"));
+        .catch((err) => {
+          console.log("err ", { err });
+          if (!err.response && err.isAxiosError)
+            return setMessage(err.message || "A Network error has occurred");
+          if (err.response && err.response.data.error.message)
+            return setMessage(err.response.data.error.message || err.message);
+        });
     };
     getCategories();
   }, []);
@@ -29,9 +35,9 @@ const Form = () => {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!data.name.trim().length > 0 || data.category.trim().length > 0) {
+    if (data.name.trim().length < 1 || data.category.trim().length < 1) {
       setMessage("Check your input");
-      return;
+      return null;
     }
     setResult((prevState) => ({
       ...prevState,
@@ -40,24 +46,75 @@ const Form = () => {
   };
   const { name } = data;
   return (
-    <form onSubmit={handleSubmit}>
-      {result.name && result.name}
-      {result.category && result.category}
-      {message && <span> {message} </span>}
-      <input name="name" value={name} onChange={handleChange} />
-      <select name="category" onChange={handleChange}>
-        <option value=""> --select--- </option>
-        {catData.length > 0 &&
-          catData.map((cat) => {
-            return (
-              <option value={cat.name} key={cat.id}>
-                {" "}
-                {cat.name}{" "}
-              </option>
-            );
-          })}
-      </select>
-      <button type="submit"> Submit </button>
+    <form
+      onSubmit={handleSubmit}
+      style={{
+        margin: "1rem",
+        padding: "1.2rem",
+      }}
+    >
+      <h3> Samuel Chibueze - Assessment work</h3>
+      <div className="data">
+        {result.name && <span> Name: {result.name} </span>} <br />
+        {result.category && <span> Category: {result.category} </span>}
+      </div>
+      <div className="messages">{message && <span> {message} </span>}</div>
+
+      <div
+        className="form-group"
+        style={{
+          marginBottom: ".8rem",
+        }}
+      >
+        <label htmlFor="name"> Name </label> <br />
+        <input
+          name="name"
+          value={name}
+          id="name"
+          onChange={handleChange}
+          className="form-control"
+          style={{
+            width: "100%",
+            padding: ".3rem",
+          }}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="category"> Choose catgory </label> <br />
+        <select
+          name="category"
+          onChange={handleChange}
+          id="category"
+          style={{
+            width: "100%",
+            padding: ".3rem",
+          }}
+        >
+          <option value=""> --select--- </option>
+          {catData.length > 0 &&
+            catData.map((cat) => {
+              return (
+                <option value={cat.name} key={cat.id}>
+                  {" "}
+                  {cat.name}{" "}
+                </option>
+              );
+            })}
+        </select>
+      </div>
+
+      <button
+        type="submit"
+        style={{
+          padding: ".4rem",
+          margin: "1rem",
+          color: "#FFF",
+          backgroundColor: "green",
+        }}
+      >
+        {" "}
+        Submit{" "}
+      </button>
     </form>
   );
 };
